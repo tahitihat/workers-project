c1fff8c8e5e9a6249550acc36dcde8a7096ccd59
Isabel Tripp <stagestruck1@gmail.com>
2020-04-20 11:32:34 -0400

    extra credit 1: changing urls/copy

--- index.js
+++ index.js
@@ -2,7 +2,37 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
-/**
+class AttributeRewriter {
+  constructor(new_copy) {
+    this.new_copy = new_copy
+  }
+
+  element(element) {
+    element.setInnerContent(this.new_copy)
+    const href = element.getAttribute('href')
+    if (href) {
+      element.setAttribute(
+        'href',
+        href.replace('https://cloudflare.com', 'http://isabeltripp.me/')
+      )
+    }
+  }
+}
+
+const text_replacements = {
+  title: 'Variant 3.14159265...',
+  h1: 'Variant 3.14159265...',
+  a: 'Learn more about me (Isabel)',
+  p: 'Notice anything different? This HTML has been parsed with HTMLRewritter! Note that the button color still reflects which variant was selected.'
+}
+
+const rewriter = new HTMLRewriter()
+  .on('title', new AttributeRewriter(text_replacements['title']))
+  .on('h1', new AttributeRewriter(text_replacements['h1']))
+  .on('a', new AttributeRewriter(text_replacements['a']))
+  .on('p', new AttributeRewriter(text_replacements['p']));
+
+/*
  * 1. Request URLs from the variants API
  * 2. Return response from randomly selected variant
  * @param {Request} request // Unused
@@ -13,13 +43,15 @@ async function handleRequest(request) {
 
   // randomly choose URL from variants array
   const arr = API_data['variants'];
-  variant_URL = arr[Math.round(Math.random())]
+  variant_URL = arr[Math.round(Math.random())];
 
   // Return response from request to selected URL
   const variant_response = await fetch(variant_URL);
   let variant_data = await variant_response.text();
-  return new Response(variant_data, {
+
+  // Transform HTML using HTMLRewriter
+  new_response = new Response(variant_data, {
     headers: { 'content-type': 'text/html' },
   });
-
+  return rewriter.transform(new_response);
 }
