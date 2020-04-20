fb2e5d248353b772ddbc8180472e6dc07cf37adc
Isabel Tripp <stagestruck1@gmail.com>
2020-04-19 23:19:16 -0400

    randomized webpage serving

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
 /**
- * Respond with hello worker text
- * @param {Request} request
+ * 1. Request URLs from the variants API
+ * 2. Return response from randomly selected variant
+ * @param {Request} request // Unused
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const API_response = await fetch('https://cfw-takehome.developers.workers.dev/api/variants');
+  let API_data = await API_response.json();
+
+  // randomly choose URL from variants array
+  const arr = API_data['variants'];
+  variant_URL = arr[Math.round(Math.random())]
+
+  // Return response from request to selected URL
+  const variant_response = await fetch(variant_URL);
+  let variant_data = await variant_response.text();
+  return new Response(variant_data, {
+    headers: { 'content-type': 'text/html' },
+  });
+
 }
